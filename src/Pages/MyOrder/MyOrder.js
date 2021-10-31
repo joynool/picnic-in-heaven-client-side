c6c34bab7b8640ea96f5b0646f5be19f2976c322
JoynoolAbedinMithoon <mithun55@gmail.com>
2021-10-31 13:25:58 +0600

    my order added

--- src/Pages/MyOrder/MyOrder.js
+++ src/Pages/MyOrder/MyOrder.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Badge, CloseButton, Table } from 'react-bootstrap';
 import useAuth from '../../hooks/useAuth';
 
 const MyOrder = () =>
 {
     const { user } = useAuth();
-    console.log(user.email)
+    //console.log(user.email)
+    const [myOrders, setMyOrders] = useState([]);
+
+    useEffect(() =>
+    {
+        fetch(`https://shielded-river-19151.herokuapp.com/order/${user.email}`)
+            .then(res => res.json())
+            .then(data => setMyOrders(data))
+            .catch(error => console.log(error));
+    }, [user.email]);
+
+    const handleMyOrderDelete = id =>
+    {
+        const proceed = window.confirm('Are you sure, you want to DELETE?');
+        if (proceed) {
+            fetch(`https://shielded-river-19151.herokuapp.com/order/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data =>
+                {
+                    if (data.deletedCount > 0) {
+                        alert('Successfully Deleted the Order');
+                        const restOrders = myOrders.filter(order => order._id !== id);
+                        setMyOrders(restOrders);
+                    }
+                })
+        }
+    }
 
     return (
         <div className="container my-5">
@@ -23,45 +51,29 @@ const MyOrder = () =>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>
-                            <Badge bg="warning" text="dark">
-                                Pending
-                            </Badge>
-                        </td>
-                        <td className="text-center"><CloseButton className="fs-5 bg-danger rounded-circle" /></td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>
-                            <Badge bg="warning" text="dark">
-                                Pending
-                            </Badge>
-                        </td>
-                        <td className="text-center"><CloseButton className="fs-5 bg-danger rounded-circle" /></td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>
-                            <Badge bg="warning" text="dark">
-                                Pending
-                            </Badge>
-                        </td>
-                        <td className="text-center"><CloseButton className="fs-5 bg-danger rounded-circle" /></td>
-                    </tr>
+                    {
+                        myOrders.map(order => <tr key={order._id}>
+                            <td>{order.orderInfo.id}</td>
+                            <td>{order.orderInfo.name}</td>
+                            <td>{order.orderInfo.price}</td>
+                            <td>{order.name}</td>
+                            <td>{order.phone}</td>
+                            <td>
+                                {
+                                    (order.orderStatus === 'pending') ? <Badge bg="warning" text="dark">
+                                        Pending
+                                    </Badge> : <Badge bg="success" text="light">
+                                        Approved
+                                    </Badge>
+                                }
+
+                            </td>
+                            <td className="d-flex justify-content-around align-items-center">
+                                <CloseButton onClick={() => handleMyOrderDelete(order._id)} className="fs-5 bg-danger rounded-circle ms-2" />
+                            </td>
+                        </tr>
+                        )
+                    }
                 </tbody>
             </Table>
         </div>
